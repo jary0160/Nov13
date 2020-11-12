3071b629905d0e918db85c0837168fd7b4d8892c
bsamra <bsamra@TNB80R9M52.scdsb.pri>
2020-11-12 01:13:41 -0500

    diamond hunter example game

---
+++ src/Car.java
@@ -0,0 +1,14 @@
+
+import org.newdawn.slick.Image;
+import org.newdawn.slick.SlickException;
+
+
+public class Car {
+    private Image image;
+    private int carXPosition = 175;
+    
+    Car()throws SlickException{
+        image = new Image("");
+    
+    }
+}
